f5e31b33f14bd83da07af3e45d825dc40b69d6c8
Oni-Bashir Atiatunnasir <97780446+Obananob@users.noreply.github.com>
2026-04-10 06:42:45 +0100

    fix: remove double rescaling in predict route

--- routes/predict_routes.py
+++ routes/predict_routes.py
@@ -21,7 +21,7 @@ async def predict(
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert("RGB")
     image = image.resize((224, 224))
-    image_array = np.array(image) / 255.0
+    image_array = np.array(image, dtype=np.float32)
     image_array = np.expand_dims(image_array, axis=0)
 
     predictions = model.predict(image_array)
